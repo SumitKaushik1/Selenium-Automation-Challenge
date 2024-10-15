5e62cd0b2dd79d52f5be5600f78443905099f0d7
Sumit-Kaushik2 <110432346+SumitKaushik1@users.noreply.github.com.>
2024-10-15 19:54:22 +0530

    new

--- src/test/java/com/listener/controltestandextendreport/One/ListenerClassExtendReportOne.java
+++ src/test/java/com/listener/controltestandextendreport/One/ListenerClassExtendReportOne.java
@@ -4,6 +4,9 @@ import com.extendedreport.one.ExtendLoggerToAddLogsInReportOne;
 import com.extendedreport.one.ExtendReporToInitAndFlushReporttOne;
 import org.testng.*;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 
 // you have to add the lisetner in the testng file definitly
 public class ListenerClassExtendReportOne implements ITestListener , ISuiteListener {
@@ -45,7 +48,33 @@ public class ListenerClassExtendReportOne implements ITestListener , ISuiteListe
     @Override
     public void onTestFailure(ITestResult result){
         try {
-            ExtendLoggerToAddLogsInReportOne.fail(result.getMethod().getMethodName()+"is failed",true);
+// Get the exception that caused the test failure
+         //   Throwable throwable = result.getThrowable();
+
+            //Convert the stack trace to a string
+         //  StringWriter sw = new StringWriter();
+           // PrintWriter pw = new PrintWriter(sw);
+            //throwable.printStackTrace(pw);;
+            //String stackTrace = sw.toString();
+            //ExtendLoggerToAddLogsInReportOne.fail(result.getMethod().getMethodName() + " is failed. Stack Trace:"+"\\n" + stackTrace, true);
+
+           // throwable.getLocalizedMessage();
+
+
+            // Get the exception that caused the test failure
+            Throwable throwable = result.getThrowable();
+
+            // Extract the exception type and the message
+            String exceptionMessage = throwable.toString(); // This will give the type and message of the exception
+
+            // Log the failure message and the brief exception message
+            ExtendLoggerToAddLogsInReportOne.fail(result.getMethod().getMethodName() + " is failed. Stack Trace: " + exceptionMessage, true);
+
+
+
+
+            // Log the failure message and the stack trace into the Extent Report
+         //   ExtendLoggerToAddLogsInReportOne.fail(result.getMethod().getMethodName() + " is failed. Stack Trace:"+"\\n" + , true);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
